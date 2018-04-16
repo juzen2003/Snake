a9add888dfd06bf37b8df6723b83e6e412d7eb58
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-15 20:12:58 -0700

    Fix typo and test function of drawing unit square

--- snake.js
+++ snake.js
@@ -5,9 +5,15 @@ const gameViewHeight = snakeCanvas.height;
 const gameViewWidth = snakeCanvas.width;
 const unitSize = 20; // the unit square size of snake
 let score = 0;
+// testing init position
+let x = 0;
+let y = 0;
+// unit move
+let dx = 1;
+let dy = 0;
 
 // draw a unit square (will be used to build a snake)
-function drawAUnitSquare(x, y) {
+function drawAUnitSquare() {
   ctx.beginPath();
   ctx.rect(x * unitSize, y * unitSize, unitSize, unitSize);
   ctx.fillStyle = "#F5FA30";
@@ -17,8 +23,13 @@ function drawAUnitSquare(x, y) {
 }
 
 function draw() {
+  // clear the screen;
+  ctx.clearRect(0, 0, snakeCanvas.width, snakeCanvas.height);
+
   // drawAUnitSquare(1,0);
-  drawAUnitSquare(0,0);
+  drawAUnitSquare();
+  x += dx;
+  y += dy;
 }
 
-setInterval(draw, 10);
+setInterval(draw, 100);
