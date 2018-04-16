bb818806ace9df36aa915063f3f6e0623413775a
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-15 20:02:47 -0700

    Add function to draw unit square in sanke.js

--- snake.js
+++ snake.js
@@ -1,2 +1,24 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+var snakeCanvas = document.getElementById("game-canvas");
+var ctx = snakeCanvas.getContext("2d");
+
+const gameViewHeight = snakeCanvas.height;
+const gameViewWidth = snakeCanvas.width;
+const unitSize = 20; // the unit square size of snake
+let score = 0;
+
+// draw a unit square (will be used to build a snake)
+function drawAUnitSquare(x, y) {
+  ctx.beginPath();
+  ctx.rect(x * unitSize, y * unitSize, unitSize, unitSize);
+  ctx.fillStyle = "#F5FA30";
+  ctx.fill();
+  ctx.stroke(); // border
+  ctx.closePath();
+}
+
+function draw() {
+  // drawAUnitSquare(1,0);
+  drawAUnitSquare(0,0);
+}
+
+setInterval(draw, 10);
